19c0963d446ad2df05f04fefae705cee5a1dfef3
mm505122 <matthieu.maxit@etu.unice.fr>
2017-11-19 20:22:44 +0100

    1

---
+++ src/utils/Vector.java
@@ -0,0 +1,19 @@
+package utils;
+
+/**
+ * Class which represent a vector in two dimensions
+ */
+public class Vector {
+
+    public int x;
+    public int y;
+
+    public Vector() {
+        this(0, 0);
+    }
+
+    public Vector(int i, int i1) {
+        this.x = x;
+        this.y = y;
+    }
+}
